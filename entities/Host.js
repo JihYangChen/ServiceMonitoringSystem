2cca0167e7c7f96c9e71af6a733c6d87d7100102
AndyChen <andy565866@gmail.com>
2018-03-25 18:16:20 +0800

    Add "Host" entity.

---
+++ entities/Host.js
@@ -0,0 +1,9 @@
+class Host {
+    constructor(displayName, host) {
+        this.displayName = displayName;
+        this.host = host;
+        this.status = status;
+        this.statusStartTime = Date();
+        this.lastCheckTime = Date();
+    }
+}
